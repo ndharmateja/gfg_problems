1cda5455856f04aa4851023b07f1442c3fe89c34
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-04-02 16:34:36 +0530

    a5: solve

---
+++ amazon/P4.java
@@ -0,0 +1,28 @@
+package amazon;
+
+public class P4 {
+    public int minDist(int a[], int n, int x, int y) {
+        int xIndex = -1;
+        int yIndex = -1;
+        for (int i = 0; i < n; i++) {
+            int val = a[i];
+            if (val == x) {
+                if (xIndex == -1 || yIndex == -1) {
+                    xIndex = i;
+                } else if (Math.abs(i - yIndex) < Math.abs(xIndex - yIndex)) {
+                    xIndex = i;
+                }
+            } else if (val == y) {
+                if (xIndex == -1 || yIndex == -1) {
+                    yIndex = i;
+                } else if (Math.abs(i - xIndex) < Math.abs(xIndex - yIndex)) {
+                    yIndex = i;
+                }
+            }
+        }
+
+        if (xIndex == -1 || yIndex == -1)
+            return -1;
+        return Math.abs(yIndex - xIndex);
+    }
+}
---
+++ amazon/P5.java
@@ -0,0 +1,24 @@
+package amazon;
+
+public class P5 {
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static void reverse(int[] a, int start, int end) {
+        int l = start;
+        int r = end;
+        while (l < r) {
+            swap(a, l++, r--);
+        }
+    }
+
+    public static void rotateArr(int[] a, int d, int n) {
+        d = d % n;
+        reverse(a, 0, n - 1);
+        reverse(a, 0, n - d - 1);
+        reverse(a, n - d, n - 1);
+    }
+}
