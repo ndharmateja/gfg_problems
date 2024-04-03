7931a8415e9af7790b3a9ce398a6f16d4dfcd62e
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-04-04 02:07:50 +0530

    a26: solve

---
+++ amazon/P26.java
@@ -0,0 +1,29 @@
+package amazon;
+
+import java.util.HashSet;
+
+public class P26 {
+    public static int solution1(int[] a, int[] b, int n, int m) {
+        HashSet<Integer> setA = new HashSet<>();
+        for (int x : a) {
+            setA.add(x);
+        }
+
+        HashSet<Integer> setB = new HashSet<>();
+        for (int x : b) {
+            setB.add(x);
+        }
+
+        int count = 0;
+        for (int x : setB) {
+            if (setA.contains(x))
+                count++;
+        }
+
+        return count;
+    }
+
+    public static int NumberofElementsInIntersection(int[] a, int[] b, int n, int m) {
+        return solution1(a, b, n, m);
+    }
+}
