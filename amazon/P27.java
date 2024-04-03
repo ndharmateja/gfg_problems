5431f13de9a6cddc2ec8f24060abf691a508150d
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-04-03 16:35:23 +0530

    p27: solve

---
+++ amazon/P27.java
@@ -0,0 +1,17 @@
+package amazon;
+
+public class P27 {
+    static int[] SortBinaryArray(int[] a, int n) {
+        int numZeroes = 0;
+        for (int x : a) {
+            if (x == 0)
+                numZeroes++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            a[i] = i < numZeroes ? 0 : 1;
+        }
+
+        return a;
+    }
+}
