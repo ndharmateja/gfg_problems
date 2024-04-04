577da7539b14d67ceaea301d63aea983fb39c2c7
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-04-04 19:00:11 +0530

    al35: solve

---
+++ amazon/linkedlist/P35.java
@@ -0,0 +1,38 @@
+package amazon.linkedlist;
+
+public class P35 {
+    static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int d) {
+            data = d;
+            next = prev = null;
+        }
+    }
+
+    // function returns the head of the linkedlist
+    Node deleteNode(Node head, int x) {
+        if (x == 1) {
+            Node newHead = head.next;
+            head.next = null;
+            newHead.prev = null;
+            return newHead;
+        }
+
+        Node curr = head;
+        for (int i = 0; i < x - 1; i++) {
+            curr = curr.next;
+        }
+
+        // Delete curr
+        curr.prev.next = curr.next;
+        if (curr.next != null)
+            curr.next.prev = curr.prev;
+        curr.prev = null;
+        curr.next = null;
+
+        return head;
+    }
+}
