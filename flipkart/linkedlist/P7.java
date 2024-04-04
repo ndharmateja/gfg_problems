3f127f35f9b99c6fbf1033741faa9c51d5415e10
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-04-04 19:47:34 +0530

    fl7: solve

---
+++ flipkart/linkedlist/P7.java
@@ -0,0 +1,54 @@
+package flipkart.linkedlist;
+
+public class P7 {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    private void insertAfter(Node node, Node toInsert) {
+        toInsert.next = node.next;
+        node.next = toInsert;
+    }
+
+    public Node mergeResult(Node head1, Node head2) {
+        // The new list
+        Node dummy = new Node(-1);
+
+        // Iterate simultaneously
+        // and attach the smaller node at the start of the new list in each iteration
+        // because we want the result in descending order
+        Node curr1 = head1;
+        Node curr2 = head2;
+        while (curr1 != null && curr2 != null) {
+            if (curr1.data < curr2.data) {
+                Node next = curr1.next;
+                insertAfter(dummy, curr1);
+                curr1 = next;
+            } else {
+                Node next = curr2.next;
+                insertAfter(dummy, curr2);
+                curr2 = next;
+            }
+        }
+
+        // Attach the remaining parts of list1 or list2 to the new list
+        while (curr1 != null) {
+            Node next = curr1.next;
+            insertAfter(dummy, curr1);
+            curr1 = next;
+        }
+        while (curr2 != null) {
+            Node next = curr2.next;
+            insertAfter(dummy, curr2);
+            curr2 = next;
+        }
+
+        return dummy.next;
+    }
+}
