dfc93f07c6ce84a5abac3bf8831c022f3d3a9a2b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-04-04 10:21:21 +0530

    al3: solve

---
+++ amazon/linkedlist/P3.java
@@ -0,0 +1,42 @@
+package amazon.linkedlist;
+
+public class P3 {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    Node sortedInsert(Node head, int key) {
+        Node newNode = new Node(key);
+        // If list is empty
+        if (head == null) {
+            return newNode;
+        }
+
+        // If key is less than or equal to the head
+        if (key <= head.data) {
+            newNode.next = head;
+            return newNode;
+        }
+
+        Node prev = null;
+        Node curr = head;
+        // If curr is null => we have to insert the key at the end
+        while (curr != null && curr.data < key) {
+            prev = curr;
+            curr = curr.next;
+        }
+
+        // At this point, curr is the first node that is greater than or equal to key
+        // so the new node should be inserted before this
+        prev.next = newNode;
+        newNode.next = curr;
+
+        return head;
+    }
+}
