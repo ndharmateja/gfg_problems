aae9df572ec2bcd9af373a6efdce915ea3b44f5c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-04-04 09:15:16 +0530

    as5: solve

---
+++ amazon/stack/P5.java
@@ -0,0 +1,43 @@
+package amazon.stack;
+
+import java.util.Stack;
+
+public class P5 {
+    class GfG {
+        public void push(int a, Stack<Integer> s) {
+            s.push(a);
+        }
+
+        public int pop(Stack<Integer> s) {
+            if (s.isEmpty())
+                return -1;
+            return s.pop();
+        }
+
+        public int min(Stack<Integer> s) {
+            if (s.isEmpty())
+                return -1;
+
+            Stack<Integer> temp = new Stack<>();
+            int min = Integer.MAX_VALUE;
+            while (!s.isEmpty()) {
+                int curr = s.pop();
+                min = Integer.min(min, curr);
+                temp.push(curr);
+            }
+
+            while (!temp.isEmpty()) {
+                s.push(temp.pop());
+            }
+            return min;
+        }
+
+        public boolean isFull(Stack<Integer> s, int n) {
+            return s.size() == n;
+        }
+
+        public boolean isEmpty(Stack<Integer> s) {
+            return s.isEmpty();
+        }
+    }
+}
