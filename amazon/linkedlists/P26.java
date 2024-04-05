9d272090837687f1c40fa100afe741cb3e699ff7
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-04-05 18:07:53 +0530

    al26: solve

--- amazon/linkedlists/P26.java
+++ amazon/linkedlists/P26.java
@@ -1,10 +1,11 @@
 package amazon.linkedlists;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 
+@SuppressWarnings("unused")
 public class P26 {
-    public static int countPairs(LinkedList<Integer> head1, LinkedList<Integer> head2,
-            int x) {
+    private static int solution1(LinkedList<Integer> head1, LinkedList<Integer> head2, int x) {
         int pairs = 0;
         for (int x1 : head1)
             for (int x2 : head2)
@@ -12,5 +13,22 @@ public class P26 {
                     pairs++;
 
         return pairs;
+    }
+
+    private static int solution2(LinkedList<Integer> head1, LinkedList<Integer> head2, int x) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int x1 : head1)
+            set.add(x1);
+
+        int pairs = 0;
+        for (int x2 : head2)
+            if (set.contains(x - x2))
+                pairs++;
+
+        return pairs;
+    }
+
+    public static int countPairs(LinkedList<Integer> head1, LinkedList<Integer> head2, int x) {
+        return solution2(head1, head2, x);
     }
 }
