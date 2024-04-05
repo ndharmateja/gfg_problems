923a641d067d6a6dce0307930b7c98124aafd468
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-04-05 11:50:40 +0530

    al24: solve

---
+++ amazon/linkedlists/P24.java
@@ -0,0 +1,42 @@
+package amazon.linkedlists;
+
+import java.util.HashSet;
+
+public class P24 {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public Node removeDuplicates(Node head) {
+        Node dummy = new Node(-1);
+        dummy.next = head;
+
+        HashSet<Integer> set = new HashSet<>();
+        Node prev = dummy;
+        Node curr = head;
+        while (curr != null) {
+            // If curr's data is already there
+            // we have to delete curr
+            // and we don't need to update prev
+            if (set.contains(curr.data)) {
+                prev.next = curr.next;
+                curr = curr.next;
+            }
+            // If we are seeing this data for the first time
+            // we add it to the set and move on
+            else {
+                set.add(curr.data);
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
