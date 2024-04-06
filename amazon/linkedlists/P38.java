f5c1d240f51bd817b3c82df04e5e463443e0e65e
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-04-06 16:44:14 +0530

    al38: refactor

--- amazon/linkedlists/P38.java
+++ amazon/linkedlists/P38.java
@@ -4,16 +4,6 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class P38 {
-    static class Node {
-        int data;
-        Node next;
-
-        Node(int a) {
-            data = a;
-            next = null;
-        }
-    }
-
     public static Node findIntersection(Node head1, Node head2) {
         HashSet<Integer> set2 = new HashSet<>();
         set2.clear();
@@ -41,44 +31,13 @@ public class P38 {
         return dummy.next;
     }
 
-    public static Node inputList(Scanner sc, int size) {
-        Node head, tail;
-        int val;
-
-        val = sc.nextInt();
-        head = tail = new Node(val);
-
-        size--;
-        while (size-- > 0) {
-            val = sc.nextInt();
-            tail.next = new Node(val);
-            tail = tail.next;
-        }
-
-        return head;
-    }
-
-    private static void print(Node head) {
-        Node curr = head;
-        while (curr != null) {
-            System.out.print(curr.data + " -> ");
-            curr = curr.next;
-        }
-        System.out.println();
-    }
-
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n, m;
-
-        n = sc.nextInt();
-        Node head1 = inputList(sc, n);
-
-        m = sc.nextInt();
-        Node head2 = inputList(sc, m);
+        Scanner s = new Scanner(System.in);
+        Node head1 = Utils.inputList(s);
+        Node head2 = Utils.inputList(s);
+        s.close();
 
         Node result = findIntersection(head1, head2);
-
-        print(result);
+        Utils.print(result);
     }
 }
