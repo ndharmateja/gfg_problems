57a293df9c82e785515753b5c3115b12d0fd0fad
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-04-06 17:11:54 +0530

    add merge and merge sort to utils

--- amazon/linkedlists/Utils.java
+++ amazon/linkedlists/Utils.java
@@ -55,13 +55,87 @@ public class Utils {
         return head;
     }
 
-    public static int getSize(Node head) {
+    public static int getSize(Node h) {
         int size = 0;
-        Node curr = head;
+        Node curr = h;
         while (curr != null) {
             size++;
             curr = curr.next;
         }
         return size;
+    }
+
+    public static void joinLists(Node h1, Node h2) {
+        Node curr = h1;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = h2;
+    }
+
+    /**
+     * splits list into 2 halves
+     * and returns the head of the second half
+     * 
+     * @param h
+     * @return
+     */
+    public static Node splitList(Node h) {
+        Node prev = null;
+        Node slow = h;
+        Node fast = h;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Break the list into 2 halves
+        prev.next = null;
+
+        // Return head of second half
+        return slow;
+    }
+
+    public static Node merge(Node h1, Node h2) {
+        // The new list
+        Node dummy = new Node(-1);
+        Node curr = dummy;
+
+        // Iterate simultaneously
+        // and attach the smaller node to the new list in each iteration
+        Node curr1 = h1;
+        Node curr2 = h2;
+        while (curr1 != null && curr2 != null) {
+            if (curr1.data < curr2.data) {
+                curr.next = curr1;
+                curr1 = curr1.next;
+            } else {
+                curr.next = curr2;
+                curr2 = curr2.next;
+            }
+            curr = curr.next;
+        }
+
+        // Attach the remaining parts of list1 or list2 to the new list
+        if (curr1 != null) {
+            curr.next = curr1;
+        }
+        if (curr2 != null) {
+            curr.next = curr2;
+        }
+
+        return dummy.next;
+    }
+
+    public static Node mergeSort(Node head) {
+        if (head == null || head.next == null)
+            return head;
+
+        Node head2 = splitList(head);
+        head = mergeSort(head);
+        head2 = mergeSort(head2);
+
+        return Utils.merge(head, head2);
     }
 }
