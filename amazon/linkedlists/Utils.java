c12d6ae9d0726e5aa1d11922f8de1ffef8869e0b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-04-06 16:39:43 +0530

    add utils fns to print and input list

--- amazon/linkedlists/Utils.java
+++ amazon/linkedlists/Utils.java
@@ -1,7 +1,9 @@
 package amazon.linkedlists;
 
+import java.util.Scanner;
+
 public class Utils {
-    public static Node reverseList(Node h) {
+    public static Node reverse(Node h) {
         Node prev = null;
         Node curr = h;
         while (curr != null) {
@@ -13,4 +15,33 @@ public class Utils {
 
         return prev;
     }
+
+    public static void print(Node h) {
+        Node curr = h;
+        while (curr != null) {
+            System.out.print(curr.data + " -> ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static Node inputList(Scanner sc) {
+        int size = sc.nextInt();
+
+        Node head, tail;
+        int val;
+
+        val = sc.nextInt();
+        head = tail = new Node(val);
+
+        size--;
+        while (size-- > 0) {
+            val = sc.nextInt();
+            tail.next = new Node(val);
+            tail = tail.next;
+        }
+
+        return head;
+    }
+
 }
