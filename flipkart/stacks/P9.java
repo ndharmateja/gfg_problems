c0610bbdfed4a08f76473b2d0b0281aa09b717dc
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-04-08 16:04:11 +0530

    fs9: refactor

--- flipkart/stacks/P9.java
+++ flipkart/stacks/P9.java
@@ -6,16 +6,12 @@ public class P9 {
     private static final char LEFT_PARANTHESIS = '(';
     private static final char RIGHT_PARANTHESIS = ')';
 
-    static boolean isOpening(char c) {
+    private static boolean isOpening(char c) {
         return c == LEFT_PARANTHESIS;
     }
 
-    static boolean isClosing(char c) {
+    private static boolean isClosing(char c) {
         return c == RIGHT_PARANTHESIS;
-    }
-
-    static boolean areMatching(char open, char close) {
-        return (open == LEFT_PARANTHESIS && close == RIGHT_PARANTHESIS);
     }
 
     public int MinParentheses(String s) {
