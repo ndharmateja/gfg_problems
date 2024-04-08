eab96c0af6a70ba8305c2d8429a51f50efdc8941
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-04-08 15:59:11 +0530

    fs9: solve

---
+++ flipkart/stacks/P9.java
@@ -0,0 +1,45 @@
+package flipkart.stacks;
+
+import java.util.Stack;
+
+public class P9 {
+    private static final char LEFT_PARANTHESIS = '(';
+    private static final char RIGHT_PARANTHESIS = ')';
+
+    static boolean isOpening(char c) {
+        return c == LEFT_PARANTHESIS;
+    }
+
+    static boolean isClosing(char c) {
+        return c == RIGHT_PARANTHESIS;
+    }
+
+    static boolean areMatching(char open, char close) {
+        return (open == LEFT_PARANTHESIS && close == RIGHT_PARANTHESIS);
+    }
+
+    public int MinParentheses(String s) {
+        int count = 0;
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (isOpening(c))
+                stack.push(c);
+            else if (isClosing(c)) {
+                // If stack is empty we need an opening bracket
+                // for the current c
+                if (stack.isEmpty())
+                    count++;
+
+                // If it is not empty
+                // we remove the matching opening paranthesis from the stack
+                else
+                    stack.pop();
+            }
+        }
+
+        // At this point if the stack is not empty
+        // all its opening brackets need closing brackets
+        count += stack.size();
+        return count;
+    }
+}
